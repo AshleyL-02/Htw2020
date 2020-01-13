003f07f03a91d968d9e242c207a3a0eebcc07b56
ASHLEY LUTY <s-ASLUTY@lwsd.org>
2020-01-12 16:16:25 -0800

    - implemented gross shooting - implemented sprite sorting in Object class

--- Htw2020/Assets/Scripts/Ash/Object.cs
+++ Htw2020/Assets/Scripts/Ash/Object.cs
@@ -5,24 +5,31 @@ using UnityEngine;
 /*
  * MUST SET HEIGHT!!!!!
  * 
+ * 
  * Used as a base class for objects (that can be collided w/?)
  * 
  * INCLUDES SPRITE SORTING
  * 
  * create InteractableObject class?
- *
  * can player duck/sit to dodge projectiles?
  * 
  */
 
 public class Object : MonoBehaviour
 {
+    //static variables
+    private static readonly string MAIN_SORTING_LAYER_NAME = "Main";    //i.e. the sorting layer where sorted objects are
+
+    //other variables
     [SerializeField]    //! temp
     private float height = 1.0f; //intended height in game space. Assumes object doesn't have holes
 
     private bool isMoving = true;   // defaults to true
 
-
+    private void Awake()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = MAIN_SORTING_LAYER_NAME;  //a precaution, if not set in the inspector
+    }
     // Start is called before the first frame update
     void Start()
     {
