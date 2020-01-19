b806f927d47b2b8f4f8f7c4fcd2dba04d52c3315
ASHLEY LUTY <s-ASLUTY@lwsd.org>
2020-01-18 22:51:11 -0800

    - fixed useless pond simulation and cherry blossoms

---
+++ Htw2020/Assets/Scripts/Ash/Pond_script/PondManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  Manages pond particle systems, for now (ie pond physics) 
+ * 
+ * this class messy af
+ */
+
+public class PondManager : MonoBehaviour
+{
+    private float pondRadius;
+    private Vector2 pondCenterCoor;
+
+    private bool gotPlayerTransform = false;
+    private bool isPaused = true;
+
+    //reference
+    private PondRipplePS pondRipplePhysicsPS;
+    private PondRipplePS pondRippleSpritePS;
+
+
+    private Transform playerTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pondRadius = this.GetComponent<CircleCollider2D>().radius;
+        pondCenterCoor = this.transform.position;
+
+        PondRipplePS[] pss = this.GetComponentsInChildren<PondRipplePS>();
+
+        pondRipplePhysicsPS = pss[0];
+        pondRippleSpritePS = pss[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gotPlayerTransform)
+        {          
+            if (isPlayerInRange())
+            {
+                this.transform.position = playerTransform.position;                
+
+                if (isPaused)
+                {
+                    pondRipplePhysicsPS.setInRange(true);
+                    pondRippleSpritePS.setInRange(true);
+
+                    isPaused = false;
+                }                
+            }
+            else
+            {
+                //ie out of range
+                isPaused = true;
+
+                pondRipplePhysicsPS.setInRange(false);
+                pondRippleSpritePS.setInRange(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(gotPlayerTransform == false && collision.GetComponent<PlayerUI>() != null)
+        {
+            Rigidbody2D playerRigidbody = collision.GetComponent<Rigidbody2D>();
+
+            pondRipplePhysicsPS.setPlayerRB(playerRigidbody);
+            pondRippleSpritePS.setPlayerRB(playerRigidbody);
+
+            playerTransform = collision.transform;
+
+            this.GetComponent<CircleCollider2D>().enabled = false;
+
+            gotPlayerTransform = true;
+        }
+    }
+
+    private bool isPlayerInRange()
+    {
+        float distanceFromCenter = ((Vector2) playerTransform.position - pondCenterCoor).magnitude;
+
+        if(distanceFromCenter <= pondRadius)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }       
+    }
+}
---
+++ Htw2020/Assets/Scripts/Ash/Pond_script/PondRipplePS.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * starts emitting when moving
+ * 
+ */
+
+public class PondRipplePS : MonoBehaviour
+{
+    private bool gotPlayer = false;
+    private bool inRange = false;
+
+    //references
+    public Rigidbody2D playerRB;
+    private ParticleSystem myParticleSystem;
+
+    private bool wasStopped = false;
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        myParticleSystem = this.GetComponent<ParticleSystem>();
+        myParticleSystem.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gotPlayer && inRange)
+        {
+            if (playerRB.velocity == Vector2.zero)
+            {
+                myParticleSystem.Stop();
+                wasStopped = true;
+            }
+            else
+            {
+                if (wasStopped == true)
+                {
+                    myParticleSystem.Play();
+                    wasStopped = false;
+                }
+
+            }
+        }
+    }
+
+    public void setPlayerRB(Rigidbody2D playerRB)
+    {
+        this.playerRB = playerRB;
+        gotPlayer = true;
+    }
+
+    public void setInRange(bool inRange)
+    {
+        this.inRange = inRange;
+    }
+
+
+}
